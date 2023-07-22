8f42501a46cfcbe3ae68f7c4b4de7d89b001d3e9
marcelovesantera <marcelorv.dm@gmail.com>
2023-07-22 16:29:23 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Movie Buster - Python e Django
+
+## Instalação dos pacotes
+
+- Abra o terminal e rode o comando:
+
+```shell
+pip install -r requirements.txt
+```
+
+## Rodando os testes
+
+- Abra o terminal e rode o pytest. Lembre-se de sempre estar com o **virtual enviroment (venv) ativado**.
+
+```python
+pytest --testdox -vvs
+```
---
+++ movies/views.py
@@ -0,0 +1,68 @@
+from rest_framework.views import APIView, Response, Request, status
+from .serializers import MovieSerializer, MovieOrderSerializer
+from .models import Movie, MovieOrder
+from django.shortcuts import get_object_or_404
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from users.permissions import IsEmployeeOrReadOnly, HasToken
+from rest_framework.pagination import PageNumberPagination
+import ipdb
+
+
+class MovieView(APIView, PageNumberPagination):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsEmployeeOrReadOnly]
+    page_size = 2
+
+    def get(self, request: Request) -> Response:
+        movies = Movie.objects.all()
+        result_page = self.paginate_queryset(movies, request)
+        serializer = MovieSerializer(result_page, many=True)
+
+        return self.get_paginated_response(serializer.data)
+
+    def post(self, request: Request) -> Response:
+        serializer = MovieSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+
+class MovieDetailView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsEmployeeOrReadOnly]
+
+    def get(self, request: Request, movie_id: int) -> Response:
+        movie = get_object_or_404(Movie, id=movie_id)
+        serializer = MovieSerializer(movie)
+
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def patch(self, request: Request, movie_id: int) -> Response:
+        movie = get_object_or_404(Movie, id=movie_id)
+
+        serializer = MovieSerializer(movie, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def delete(self, request: Request, movie_id: int) -> Response:
+        movie = get_object_or_404(Movie, id=movie_id)
+        movie.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class MovieOrderView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [HasToken]
+
+    def post(self, request: Request, movie_id: int) -> Response:
+        movie_obj = get_object_or_404(Movie, id=movie_id)
+
+        serializer = MovieOrderSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(movie=movie_obj, user=request.user)
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
